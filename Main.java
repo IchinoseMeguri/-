b77083e1d26ad85dad924fe7f4e9f1afe5f39631
IchinoseMeguri <1577719837@qq.com>
2022-04-07 10:47:27 +0800

    First commit.

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*
+ * @Author: Meguri Ichinose
+ * @Date: 2022-04-07 10:46:12
+ * @Description: 
+ */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("云计算技术与分布式系统\n第一次实验\n测试文件");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# -
+<!--
+ * @Author: Meguri Ichinose
+ * @Date: 2022-04-07 10:44:49
+ * @Description: 
+-->
+# First-experience
 云计算与分布式系统第一次实验
